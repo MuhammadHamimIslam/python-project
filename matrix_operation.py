b8697effcb44ea5f6faf5908c9c8cafae9a348f1
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-03-26 14:32:49 +0600

    update matrix_operation.py

--- matrix_operation.py
+++ matrix_operation.py
@@ -1,11 +1,21 @@
 def takeInp():
-    row = int(input("Enter the row number: ")) 
-    col = int(input("Enter the column number: "))
+    while True: # to get tje input 
+        try: # input validation 
+            row = int(input("Enter the row number: ")) 
+            col = int(input("Enter the column number: "))
+            break # after taking input, break
+        except ValueError:
+             print("Row and column must be an integer!")
     result = []
     for i in range(row): 
         rows = []
         for j in range(col):
-            inp = int(input(f"Enter the value in {i}×{j}: "))
+            while True: # loop to get the input
+                try: # input validation 
+                    inp = int(input(f"Enter the value in {i + 1}×{j + 1}: "))
+                    break # after taking input, break 
+                except ValueError:
+                    print("Matrix value must be an integer!")
             rows.append(inp)
         result.append(rows)
     return result
@@ -38,8 +48,8 @@ def subsMatrix(m1, m2):
 
 def dotProduct(m1, m2):
     # error check 
-    if len(m1) != len(m2[0]): 
-        return "Can't perform matrix multiplication!"
+    if len(m1[0]) != len(m2): 
+        return "To perform matrix multiplication, the number of columns in the first matrix should be equal to the number of rows in the second matrix."
     result = [[0 for _ in range(len(m1))] for _ in range(len(m2[0]))]
         # loop to iterate over the rows of A
     for i in range(len(m1)): 
@@ -56,7 +66,7 @@ def matrixOperation(matrix1, matrix2):
         print(sumMatrix(matrix1, matrix2))
     elif inp == "-":
         print(subsMatrix(matrix1, matrix2))
-    elif inp == "." or inp == "×" or inp == "*":
+    elif inp in ("*", "×", "."):
         print(dotProduct(matrix1, matrix2))
     else:
         print("Invalid operator")

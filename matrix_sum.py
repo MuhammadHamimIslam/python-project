b985aacdeaad569f2ebeb6866d55a5987659b992
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-03-26 14:13:29 +0600

    update matrix_sum.py

--- matrix_sum.py
+++ matrix_sum.py
@@ -1,4 +1,6 @@
-def takeInp(row, col): 
+def takeInp():
+    row = int(input("Enter the row number: ")) 
+    col = int(input("Enter the column number: "))
     result = []
     for i in range(row): 
         rows = []
@@ -11,48 +13,57 @@ def takeInp(row, col):
 def sumMatrix(m1, m2):
     if len(m1) != len(m2): 
         return "To perform matrix sum, Both length should be equal."
+    if len(m1[0]) != len(m2[0]): 
+        return  "To perform matrix sum, Both length should be equal."
     result = []
     for i in range(len(m1)):
         rows = []
         for j in range(len(m1[0])): 
             rows.append(m1[i][j] + m2[i][j])
         result.append(rows)
-    print(result)
     return result
 
 def subsMatrix(m1, m2):
     if len(m1) != len(m2): 
-        return "To perform matrix sum, Both length should be equal."
+        return "To perform matrix substitution, Both length should be equal."
+    if len(m1[0]) != len(m2[0]): 
+        return  "To perform matrix substitution, Both length should be equal."
     result = []
     for i in range(len(m1)):
         rows = []
         for j in range(len(m1[0])): 
             rows.append(m1[i][j] - m2[i][j])
         result.append(rows)
-    print(result)
     return result
+
+def dotProduct(m1, m2):
+    # error check 
+    if len(m1) != len(m2[0]): 
+        return "Can't perform matrix multiplication!"
+    result = [[0 for _ in range(len(m1))] for _ in range(len(m2[0]))]
+        # loop to iterate over the rows of A
+    for i in range(len(m1)): 
+        # loop to iterate over the cols of B
+        for j in range(len(m2[0])): 
+            # loop to iterate over the rows of B
+            for k in range(len(m2)): 
+                result[i][j] += m1[i][k] * m2[k][j] # now update the result 
+    return result # return result 
 
 def matrixOperation(matrix1, matrix2): 
     inp = input("Enter the operator: ")
     if inp == "+": 
-        sumMatrix(matrix1, matrix2)
+        print(sumMatrix(matrix1, matrix2))
     elif inp == "-":
-        subsMatrix(matrix1, matrix2)
-        pass
+        print(subsMatrix(matrix1, matrix2))
+    elif inp == "." or inp == "×" or inp == "*":
+        print(dotProduct(matrix1, matrix2))
     else:
-        return "Invalid operator"
+        print("Invalid operator")
 
-row = int(input("Enter the row number: ")) 
-col = int(input("Enter the column number: "))
+print("Enter Value for matrix 1")    
+matrix1 = takeInp()
+print("Enter Value for matrix 2")
+matrix2 = takeInp()
 
-if row == 0: 
-    print("Length can't be zero!")
-elif col == 0:
-    print("Length can't be zero!")    
-else:
-    print("Enter Value for matrix 1")    
-    matrix1 = takeInp(row, col)
-    print("Enter Value for matrix 2")
-    matrix2 = takeInp(row, col)
-
-    matrixOperation(matrix1, matrix2) 
+matrixOperation(matrix1, matrix2)

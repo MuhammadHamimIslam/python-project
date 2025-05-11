33495ca1febb57f6ce336538c2e056531f9aad5a
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-05-11 15:47:07 +0600

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,21 +1,42 @@
+# user input taking function 
+def take_input(prompt):
+    while True:
+      try:
+          input_number = float(input(prompt))
+          return input_number
+          break
+      except ValueError:
+          print("Enter a valid value!")
+        
+# function to perform calculation 
+def calculate(num1, num2, operator):
+    if operator == "+": return num1 + num2
+    elif operator == "-": return num1 - num2
+    elif operator in ("*", "×"): return num1 * num2
+    elif operator in ("/", "÷"): return num1 / num2
+    elif operator in ("**", "^"): return num1 ** num2
+    else: return "Invalid operator!"
+    
 
-# program design 
-while True:
-    number1 = float(input("Enter number 1: "))
-    number2 = float(input("Enter number 2: "))
-    operator = input("Enter the operator: ")
-    if not operator: 
-        break
-    if operator == "+":
-        print("sum is:  %f", number1 + number2)
-    elif operator == "-":
-        print("Result is: %f", number1 - number2)
-    elif operator in ("*", "×") :
-        print("Result is: %f", number1 * number2)
-    elif operator in ("/", "÷"):
+# main program design 
+def main(): 
+    while True:
+        operator = input("Enter the operator: (+, -, (*, ×), (/, ÷), (**, ^) or q to quit: ").strip() # get the input and trim the whitespaces 
+        # if user enter q -> quit the program
+        if operator.lower() == "q": 
+            break 
+        # take user input 
+        num1 = take_input("Enter number 1: ")
+        num2 = take_input("Enter number 2: ")
+        # a try block of performing calculation 
         try:
-            print("Result is: %f", number1 / number2)
-        except ZeroDivisionError:
-            print("Can't divide by 0!")
-    else:
-        print("Invalid operator!")
+            print(calculate(num1, num2, operator))
+        except Exception as e:
+            print(e)
+        
+
+
+
+
+if __name__ == '__main__':
+    main()

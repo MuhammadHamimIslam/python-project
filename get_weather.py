3e9a4b2e76f8c025bd2ae38dfdfb775168247155
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-06-07 09:43:47 +0600

    update get_weather.py

---
+++ get_weather.py
@@ -0,0 +1,15 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("WEATHER_API")
+city = input("Enter the city: ")
+
+res = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}")
+
+if res.status_code == 200: 
+    print(res.text)
+else:
+    print("Error: ", res.status_code)
